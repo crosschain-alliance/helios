33d18044789bbb460b9883e9d95a783ba2b6f362
zengzengzenghuy <tayzh2799@gmail.com>
2025-03-04 17:00:25 +0100

    chore: add checkpoint for Gnosis

--- ethereum/src/config/checkpoints.rs
+++ ethereum/src/config/checkpoints.rs
@@ -12,7 +12,7 @@ use serde::{
 use crate::config::networks;
 
 /// The location where the list of checkpoint services are stored.
-pub const CHECKPOINT_SYNC_SERVICES_LIST: &str = "https://raw.githubusercontent.com/ethpandaops/checkpoint-sync-health-checks/master/_data/endpoints.yaml";
+pub const CHECKPOINT_SYNC_SERVICES_LIST: &str = "https://raw.githubusercontent.com/ethpandaops/crosschain-alliance/helios/tree/feat/gnosis-backup/checkpoint.yaml";
 
 #[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct RawSlotResponse {
@@ -108,6 +108,7 @@ impl CheckpointFallback {
                 networks::Network::Mainnet,
                 networks::Network::Sepolia,
                 networks::Network::Holesky,
+                networks::Network::Gnosis,
             ],
         }
     }
